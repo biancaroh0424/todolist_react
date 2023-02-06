7f06407eb19a24958cc10776a89b24dd0cce5e3a
Bianca Roh <80821859+biancaroh0424@users.noreply.github.com>
2023-02-06 17:15:03 +0900

    dd

--- src/App.js
+++ src/App.js
@@ -1,31 +1,103 @@
-import './App.css';
 import {db} from './firebase';
-import {collection, getDocs} from 'firebase/firestore';
+import {collection, connectFirestoreEmulator, doc, getDocs, addDoc, updateDoc, deleteDoc, query, orderBy} from 'firebase/firestore';
+import './App.css';
 import {useEffect, useState} from 'react'
+import { toHandlers } from 'vue';
 
 
 //파이어베이스에서 데이터 가져오기
 
 
 function App() {
-  const [todos, setLists] = useState([]);
-  const todosCollectionRef = collection(db, "todos");
+
+  const [changed, setChanged] = useState(false)
+
+  const [todos, setList] = useState([]);
+  const todosCollectionRef = collection(db, "todolist-react-bianca");
+  const [newList, setNewList] = useState("")
+  console.log(newList)
+
 
   useEffect( ()=>{
-
     const getLists = async()=>{
-      const data = await getDocs(todosCollectionRef);
+      const data = await getDocs(
+        query(todosCollectionRef, orderBy("timeStamp", "desc"))
+        );
       //console.log(data)
-      setList(data.docs.map(doc => ({...doc.data(), id: doc.id})));
-    }
+      setList(data.docs.map(
+          (doc)=>(
+          {...doc.data(), id:doc.id}
+          ))
+      )}
 
-    getLists()
-  }, [])
+    getLists();
+    setChanged(false)
+  }, [changed])
 
+var date = new Date();
+const now_date = date.getFullYear()+"-"+(date.getMonth()+1)+
+"-"+date.getDate();
+
+
+  const createList = async ()=>{
+    //await addDoc(연결객체, 전달할 값)
+    await addDoc(todosCollectionRef,{things:newList, d_date:now_date, timeStamp:date})
+    // alert(now_date)
+    setChanged(true);
+  }
+
+
+
+const updateList = async (id,things)=>{
+console.log(id + '/' + things)
+const msg = window.prompt("TODO",things)
+
+if(msg){
+  // id 를 이용하여 업데이트 할 데이터 검색
+  const listDoc = doc(db, "todolist-react-bianca", id);
+  const editField = {
+    things:msg,  d_date:now_date, 
+    timeStamp:new Date()}
+  //updateDoc(어떤 데이터, 어떤 값) 데이터 업데이트
+  await updateDoc(listDoc, editField)
+  setChanged(true);
+}
+
+}
+
+const deleteList = async (id)=>{
+  const confirmMessage = window.confirm("Are you sure you want to delete this list?");
+  
+  if(confirmMessage){
+    const listDoc = doc(db, "todolist-react-bianca", id);
+    await deleteDoc(listDoc)
+    setChanged(true);
+  }
+}
+
+
+
+const showList = todos.map(
+  (values) => (
+    <div key={values.id} className="row">
+        <h2>{values.things}</h2>
+            <span className="date">
+                {values.d_date}
+            </span>
+            <button onClick={()=>{updateList(values.id, values.content)}}>수정</button>
+            <button onClick={()=>{deleteList(values.id)}}>삭제</button>
+    </div>
+))
 
 
   return (
     <div className="App">
+      <input type="text" placeholder="todos..." onChange={
+        (event)=>{setNewList(event.target.value)}
+      }/>
+      <button onClick={createList}>Add List</button>
+      <hr />
+      {showList}
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 
 import { initializeApp } from "firebase/app";
-import { Firestore } from "@firebase/firestore";
+import { getFirestore } from "@firebase/firestore";
+
 
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -14,23 +15,24 @@ import { Firestore } from "@firebase/firestore";
 
 
 const firebaseConfig = {
-apiKey: "AIzaSyBDCNQSXdmU8cOXf9qxjtY73HBHkasrNiQ",
-authDomain: "todolist-react-bianca.firebaseapp.com",
-projectId: "todolist-react-bianca",
-storageBucket: "todolist-react-bianca.appspot.com",
-messagingSenderId: "606295496468",
-appId: "1:606295496468:web:17a22214ac17c8bca0d401"
-};
+
+    apiKey: "AIzaSyBDCNQSXdmU8cOXf9qxjtY73HBHkasrNiQ",
+    authDomain: "todolist-react-bianca.firebaseapp.com",
+    projectId: "todolist-react-bianca",
+    storageBucket: "todolist-react-bianca.appspot.com",
+    messagingSenderId: "606295496468",
+    appId: "1:606295496468:web:17a22214ac17c8bca0d401"
+  
+  };
+  
 
 
 
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
 
 
 // 파이어베이스 DB객체 중에서 todos 항목을 다른 곳에서 사용하도록 공개
-export const oTodosinDB = oDB.ref('todos');
-
 
 export const db = getFirestore(app)
