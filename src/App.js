126af59e3fe319cb0b56682e687c83648d8aa80e
Bianca Roh <80821859+biancaroh0424@users.noreply.github.com>
2023-02-06 15:20:48 +0900

    ..

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import {db} from './firebase';
+import {collection, getDocs} from 'firebase/firestore';
+import {useEffect} from 'react'
+
+//파이어베이스에서 데이터 가져오기
+
 
 function App() {
+
+  const todosCollectionRef = collection(db, "todos");
+
+  useEffect( ()=>{
+
+    const getLists = async()=>{
+      const data = await getDocs(todosCollectionRef);
+      console.log(data)
+    }
+
+  }, [])
+
+  getLists()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
